135363cb9dc8a50bd57d32ea96bc0b741a006341
Student <student@c012.local>
2018-11-20 17:01:09 +0300

    Создали пользовательский интерфейс

--- Apple_Pie_Shagina/ViewController.swift
+++ Apple_Pie_Shagina/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet weak var correctWordLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
+    
 
 }
 
